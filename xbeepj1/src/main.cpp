adde65a5c33e015ba3f484f50c9823d5b55d66cf
gleissonjoaquim3 <gleissoncg2@gmail.com>
2020-01-16 23:31:24 -0300

    Códigop em funcionamento

--- xbeepj1/src/main.cpp
+++ xbeepj1/src/main.cpp
@@ -1,58 +1,82 @@
-/* Output-side (LED) Arduino code */
-#include <SoftwareSerial.h>
-#include <Arduino.h>
-// RX: Arduino pin 2, XBee pin DOUT.  TX:  Arduino pin 3, XBee pin DIN
-SoftwareSerial XBee(2, 3);
-const int LED1 = 9;  //Declaração do led1 no pino 9
-//const int LED2 = 10; //Declaração do led2 no pino 10
-//const int LED3 = 11; //Declaração do led3 no pino 11
-const int bto1 = 8;  //Declaração do bot1 no pino 8
-//const int bto2 = 7;  //Declaração do bot2 no pino 7
-//const int bto3 = 6;  //Declaração do bot3 no pino 6
-int estadobot1 = 0;
-//int estadobot1 , estadobot2 , estadobot3 = 0; //Variável que conterá os estados do botão (0 LOW, 1 HIGH).
+/* Código para que seja enviado um sinal via placa xbee para outro dispositivo xbee
+//que ao receber este sinal envia outro como resposta */
 
+#include <SoftwareSerial.h> // Inclusão de biblioteca
+#include <Arduino.h> // Inclusão de biblioteca
+
+SoftwareSerial XBee(2, 3); // RX: Arduino pin 2, XBee pin DOUT.  TX:  Arduino pin 3, XBee pin DIN
+#define pinbotao 12 // Botão definido na entrada digital 12
+#define ledvm 7 // LED definido na saída digital 7
+#define ledvd 6 // LED definido na saída digital 6
+
+boolean estadobotao = false; // Definido estado da variavel estadodobotao tipo verdadeiro ou falsa
+boolean estantbotao = false; // Definido estado da variavel estantbotão na variavel tipo verdadeiro ou falsa
+boolean pisca = false; // Definido estado da variavel pisca tipo verdadeiro ou falsa
+unsigned long delay1 = 0; // Definido variavel delay1 com início em 0
+// tipo de declaração que pode armazenar maior quantidade de números  
 
 void setup()
-{
-  // Baud rate MUST match XBee settings (as set in XCTU)
-  XBee.begin(9600);
-  Serial.begin(9600);
-  pinMode(LED1,OUTPUT); //Led 1 configurado como saída
-  //pinMode(LED2,OUTPUT); //Led 2 configurado como saída
-  //pinMode(LED3,OUTPUT); //Led 3 configurado como saída
-  pinMode(bto1,INPUT); //bto1 configurado como entrada
-  //pinMode(bto2,INPUT); //bto2 configurado como entrada
-  //pinMode(bto3,INPUT); //bto3 configurado como entrada
+{ // Código que será carregado ao ligar o arduino
+  pinMode(pinbotao, INPUT); // Variavel pinbotao definida como entrada(INPUT)
+  //pinMode(LED_BUILTIN,OUTPUT); 
+  pinMode(ledvd,OUTPUT); // Variavel ledvd definida como entrada(OUTPUT)
+  pinMode(ledvm,OUTPUT); // Variavel ledvd definida como entrada(OUTPUT)
+  XBee.begin(9600); // Inicia o serial de leitura do xbee no BAUD RATE 9600
+  //Serial.begin(9600); // Inicia o serial monitor no BAUD RATE 9600
 }
 
 void loop()
-{
-  //Faz a leitura do estado do pino 2, constante botao, e atribuindo 
-  //o resultado a variável estadoBotao.
-  estadobot1 = digitalRead(bto1);
-  //estadobot2 = digitalRead(bto2);
-  //estadobot3 = digitalRead(bto3);    
+{ // Será repetido infinitamente pelo arduino
+  XBee.available();  // Verifica se há algum xbee disponivel
+  char c = XBee.read(); // Armazena na variavel c o sinal recebido
+  //Serial.print(c);
+  estadobotao = digitalRead(pinbotao); // estadobotao recebe o sinal do botão
+  
+  if (estadobotao == HIGH) // se o botão for pressionado
+  {
+    XBee.write('K'); // Manda um caracter 'L' para outro disposisivo
+  } 
 
-  if (XBee.available())  //Verifica se tem outros xbee disponiveis
-  { 
-    char c = XBee.read(); // Faz a leitura enviada por outro xbee
-    
-    if (c == 'H' && estadobot1 == LOW) //Caso receba a letra H e o botão não esteja pressuinado
-    {
-      digitalWrite(LED1, HIGH); // Acende o led
-      Serial.print("Chamada berço 1"); // No Seria monitor printa a Identificação do berço 1 por exemplo
-      delay (1000);
-      digitalWrite(LED1,LOW);
-      delay(1000); // Delay de 4s
-    }
-    else
-    {
-    digitalWrite(LED1, LOW);
-    XBee.write("R");
-    }
-    
+  if (estadobotao && !estantbotao ) // Condição que compara o estado atual com o estado anterior do botão
+  {
+    pisca = !pisca; // troca o estaco da variavel pisca para true
   }
   
-}
+  if (c == 'k' && (pisca)) // Condição caso receba o caracter correto e o pisca for false
+  {
+    pisca = !pisca;
+    digitalWrite(ledvm,LOW);
+    digitalWrite(ledvd,HIGH);
+    delay(40000);
+    digitalWrite(ledvd,LOW); // Acende o led verde por x segundos e apaga o led vermelho
+
+  }
   
+   // Aqui digo que caso o botao seja acionado entrará em modo de pisca
+  if (pisca ) // se a variavel pisca for true
+  
+  {      
+    if ((millis() - delay1) >= 700) //Função millis Retorna o número de milissegundos passados desde 
+    {                                  //que a placa Arduino começou a executar o programa atual.
+                                      // Se a diferença entre a condição for maior ou igual a x. Executa algo 
+      digitalWrite( ledvm, HIGH); // Acende o led vermelho
+    }
+
+    if ((millis() - delay1) < 700) //Se a diferença entre a condição for menor a x. Executa algo 
+    {
+      digitalWrite( ledvm, LOW); // Apaga o led vermelho.
+    }
+    
+    if ((millis() - delay1) >= 1400)
+    {
+      delay1 = millis(); // Faz com que a função millis retorne ao valor 0
+    }
+
+  } 
+  else 
+  {
+    digitalWrite( ledvm, LOW);
+  }
+    
+delay(200);
+}
