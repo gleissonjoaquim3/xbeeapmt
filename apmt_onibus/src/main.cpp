be58d9b5c29d3e88cd16ab8563aa2c54b663583b
gleissonjoaquim3 <gleissoncg2@gmail.com>
2020-02-19 14:29:21 -0300

    Correções__

--- apmt_onibus/src/main.cpp
+++ apmt_onibus/src/main.cpp
@@ -1,11 +1,14 @@
+/* Programa destinado ao recebimento de sinal que será emitido
+pelos postos remotos localizados ao longo do Terminal Portuário */
+
 #include <SoftwareSerial.h> // Inclusão de biblioteca
 #include <Arduino.h> // Inclusão de biblioteca
 
 SoftwareSerial XBee(2, 3); // Criação do objeto XBee
 
 #define pinbotao_1 5  //Definição do botão no pino 5
-#define pinbotao_2 6
-#define pinbotao_3 7
+//#define pinbotao_2 6
+//#define pinbotao_3 7
 #define ledvm1 8    //Definição do botão no pino 8
 //#define ledvm2 9
 //#define ldevm3 10
@@ -28,28 +31,25 @@ void pega_caracter(const char letra_a_receber,
     int led, int botao,
     int estadobotao)
 {
-XBee.available(); // Verifica a disponibilidade de outros dispositivos
-char c = XBee.read(); // Faz a leitura de sinal enviado
-//XBee.print(c); // Descomentar para ver pelo serial o sinal recebido
 
 estadobotao = digitalRead(botao); // Recebe o sinal do botao
 
+if ( XBee.available() > 0) // Verifica a disponibilidade de outros dispositivos
+{
+  char c = XBee.read(); // Faz a leitura de sinal enviado
+//XBee.print(c); // Descomentar para ver pelo serial o sinal recebido
 if (c == letra_a_receber)
 {
   digitalWrite(led, HIGH); // Aciona o led
-
-  if (estadobotao == HIGH)
-  {
-    XBee.write(letra_a_enviar); // Escreve na saida xbee
-    digitalWrite(led, LOW); // Desliga o led
-  }
-
+}
+if (c == letra_a_receber && estadobotao == HIGH)
+{
+  XBee.write(letra_a_enviar); // Escreve na saida xbee
+  digitalWrite(led, LOW); // Desliga o led
+}
 /*TODO Estudar uma forma de fazer um tipo de envio que possa "buscar"
 o outro dispositivo */
-
 }
-
-
 }
 
 void setup() // Código carregado apenas uma vez.
@@ -67,5 +67,6 @@ void loop() // Código qe será executado sempre pelo arduino
 {
 
 pega_caracter('K', 'k', ledvm1, pinbotao_1, estadobotao_1);
+delay(60);
 
 }
