0456a9f3b45886aa4b1963ecc930d5adb91257f1
Gleisson Jesuino Joaquim Cardoso <gcardoso@MDebian>
2020-06-15 20:46:00 -0300

    Update header main.cpp

--- apmt_onibus/src/main.cpp
+++ apmt_onibus/src/main.cpp
@@ -1,4 +1,5 @@
-/* Copyright (C) 2020  Gleisson J. J. Cardoso
+/* Copyright (C) 2020  Gleisson J. J. Cardoso <gleissoncg2@gmail.com>
+ *               2020  Robson Cordeiro  <robson.cordeiro@apmt.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
