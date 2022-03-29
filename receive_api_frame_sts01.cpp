3a42dbff6ad886c13b652b73f27efdaf06cb9dbd
gleissonjoaquim3 <55757359+gleissonjoaquim3@users.noreply.github.com>
2022-03-29 11:06:37 -0300

    Add example files

---
+++ receive_api_frame_sts01.cpp
@@ -0,0 +1,94 @@
+/* Copyright (C) 2020  Gleisson J. J. Cardoso <gleissoncg2@gmail.com>
+ *               
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+/* Código para que seja enviado um sinal via placa xbee shield conectada a um arduino
+   para outro dispositivo xbee conectado a outro arduino Uno. Nas saídas digitais 8 e 9 serão conectadas
+   a módulos reles, que trabalham com tensão inversa de acionamento.
+   Nota: Nesse sketch a shield está utilizando os pinos 2 3 para execução das transmissões RX e TX.
+   Sendo assim a necessidade da declaração dos respectivos pinos.*/
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee (2, 3); // Definição dos pinos RX e TX. Pois nesta shield possui um jumper externo.
+
+#define led 13 // Led interno do Arduino
+//Definição dos pinos de saída. 
+#define led_VD 8 // 
+#define led_VM 9
+
+
+//Frames recebidos pelo xbee do emissor.
+//7E 00 10 10 01 00 13 A2 00 41 BE 39 E2 FF FE 00 00 10 50 C2
+//7E 00 10 10 01 00 13 A2 00 41 BE 39 E2 FF FE 00 00 10 60 B2
+//7E 00 10 10 01 00 13 A2 00 41 BE 39 E2 FF FE 00 00 10 70 A2
+
+//TODO Trocar os frames que serão enviados para o segundo XBee
+
+
+//Variavel que armazenará valor lido recebido pelo xbee.
+int valor_lido = 0;
+
+void setup()
+{
+  //Inicialização da Serial no Baud Rate 9600
+  Serial.begin(9600);
+  xbee.begin(9600);
+  //Definição dos pinos de saída
+  pinMode(led, OUTPUT);
+  pinMode(led_VD, OUTPUT);
+  pinMode(led_VM, OUTPUT);
+  // Setadas as saídas no valor alto, para que os reles iniciem o programa desligados.
+  digitalWrite(led_VD, HIGH);
+  digitalWrite(led_VM, HIGH); 
+  }
+
+void loop() // Será executado pelo arduino constantemente
+{
+if (xbee.available() >=19) { // Fica constantemente na escuta do RX
+
+if (xbee.read() == 0x7E){ // Se o frame lido começar com o byte inicial 0x7E
+//Pula os byte do API frame que nós não queremos
+for (int i =0; i<16; i++) {
+  byte skip = xbee.read();
+}
+   valor_lido = xbee.read();
+   //Serial.print(valor_lido); // Usado para debug para mostrar o valor lido pela variável
+   //Serial.print("Chegou a mensagem..."); // Usado para debug
+   
+  switch (valor_lido) // Muda para o caso em que o valor seja correspondente.
+{
+case 77:
+  digitalWrite(led_VM, LOW); // Liga led_VM
+  //Serial.println("Acender o LED !! "); //// Usado para debug
+  delay(50);
+  // Finaliza a ação.
+  break;
+case 93:
+  // Liga led_VD e desliga led_VM
+  digitalWrite(led_VM, HIGH);
+  digitalWrite(led_VD, LOW);
+  //Serial.println("Apagar o LED !! "); // Usado para debug
+  delay(50);
+  // Finaliza a ação.
+  break;
+case 61:
+  // Desliga o led_VD
+  digitalWrite(led_VD, HIGH);
+  delay(50);
+  // Finaliza a ação.
+  break;
+}
+}
+}
+} 
